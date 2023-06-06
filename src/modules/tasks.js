db4a1b2624e244d668973ef9dbb5963f79a328ab
moFuhidy <mo7ammed9290@gmail.com>
2023-06-06 16:57:09 +0300

    added a function for editing task descriptions

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -22,15 +22,32 @@ export default class Tasks {
     let singleTask = '';
     tasksList.forEach((task) => {
       singleTask += `<li class="task-item" id='${task.index}'>
-                      <div class="checkList"> <input type="checkbox" name="check" class="chcek"> ${task.description}</div>
+                      <div class="checkList">
+                       <input type="checkbox" name="check" class="chcek">
+                       <!-- put the description inside input so the user can edit it -->
+                       <input class='inputDesc' value="${task.description}">
+                        </div>
                       <i class="fa-solid fa-trash delete"></i>
                   </li>`;
       listContainer.innerHTML = singleTask;
     });
+    // Implement the delete task
     const delButton = document.querySelectorAll('.delete');
     delButton.forEach((delBtn, i) => {
       delBtn.addEventListener('click', () => {
         Tasks.removeTask(i);
+      });
+    });
+
+    // Implement the editing task
+    const editTaskInput = document.querySelectorAll('.inputDesc');
+
+    editTaskInput.forEach((editTask, i) => {
+      editTask.addEventListener('change', () => {
+        if (editTask.value) {
+          tasksList[i].description = editTask.value;
+          localStorage.setItem('todo', JSON.stringify(tasksList));
+        }
       });
     });
   };
