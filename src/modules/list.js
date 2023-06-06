58ecd7f8308b7d024dd9787e7935616431cb7ed0
moFuhidy <mo7ammed9290@gmail.com>
2023-06-06 13:27:48 +0300

    added the tasks module

---
+++ src/modules/list.js
@@ -0,0 +1,19 @@
+const tasksList = [
+  {
+    description: 'Organize digital files',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Clean the house',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Plan a day trip',
+    completed: false,
+    index: 3,
+  },
+];
+
+export default tasksList;
