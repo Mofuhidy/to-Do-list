b12cce46ea7b1096b8340e80522a95461f104244
moFuhidy <mo7ammed9290@gmail.com>
2023-06-14 11:55:00 +0300

    Added the add function test

---
+++ src/test.js/addRemove.test.js
@@ -0,0 +1,26 @@
+import addTask from '../__mocks__/addTask.js';
+
+describe('test add and remove functions', () => {
+  test('Add one new item to the list', () => {
+    // Arrange
+    document.body.innerHTML = `<section class="listCont">
+            <div class="title">Today's To Do <i class="fa-solid fa-arrows-rotate refresh"></i></div>
+            <div id="add">
+                <input type="text" placeholder="Add to your list..." name="addDo" id="addDo">
+                <i class="fa-solid fa-arrow-turn-down enter"></i>
+            </div>
+            <ul class="tasks-container">
+            </ul>
+            <div class="clearCont">
+                <button class="clrBtn">Clear all completed</button>
+            </div>
+        </section>`;
+    // Act
+    const add = document.querySelector('#addDo');
+    add.value = 'hello';
+    addTask();
+    // Assert
+    const list = document.querySelectorAll('.tasks-container li');
+    expect(list).toHaveLength(1);
+  });
+});
